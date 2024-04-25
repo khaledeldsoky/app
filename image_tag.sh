95f1e7a3378f0b4c67d9ba4cb9480d033fba6a35
khaledeldsoky <khlaedmohamedeldsoky@gmail.com>
2024-04-25 15:13:55 +0200

    change bash to run dirct in cd branch

--- image_tag.sh
+++ image_tag.sh
@@ -3,4 +3,4 @@ set -x
 COMMIT=`git rev-parse HEAD | cut -c -7`
 echo $COMMIT
 # sed -E -i'' "s/(.*nightwolf:).*/\1${COMMIT}/" $1
-sed -i "s/khaledmohamedatia\/app:*/khaledmohamedatia\/app:${COMMIT}/g" $1
+sed -i "s/khaledmohamedatia\/app:*/khaledmohamedatia\/app:${COMMIT}/g" ../cd/deployment.yml
